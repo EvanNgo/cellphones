72a41472493db5fe5890f978cf6a61c25a411ce6
Evan <evanngo93@gmail.com>
2019-11-05 23:08:23 +0900

    Header SP

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once DOC_ROOT.'/common/php/common.php';
     <?=insertCSS('/top/css/style.css');?>
   </head>
   <body>
-    <header>
+    <header class="only-pc">
       <div class="Responsive">
         <div class="Menu">
           <div class="Menu-branch">
@@ -19,7 +19,11 @@ require_once DOC_ROOT.'/common/php/common.php';
             </div>
             <div class="Menu-branch-search">
               <div class="Menu-branch-search-location">
-                <p class="Menu-branch-search-location__txt">Hà Nội</p>
+                <p class="js-locationBtn Menu-branch-search-location__txt">Hà Nội</p>
+                <div class="Menu-branch-search-location-select">
+                  <p class="js-locationOption"><i class="fa fa-map-marker"></i><span>Hà Nội</span></p>
+                  <p class="js-locationOption"><i class="fa fa-map-marker"></i><span>Hồ Chí Minh</span></p>
+                </div>
               </div>
               <div class="Menu-branch-search-input">
                 <input type="text" placeholder="tìm kiếm">
@@ -35,10 +39,35 @@ require_once DOC_ROOT.'/common/php/common.php';
                 <div class="Menu-list-item-content__ico"><i class="fas fa-mobile-alt"></i></div>
                 <p class="Menu-list-item-content__txt">ĐIỆN THOẠI</p>
               </div>
+              <div class="Menu-list-item-sub">
+                <div class="Menu-list-item-sub-list">
+                  <div class="Menu-list-item-sub-item">
+                    <a href="#" class="Menu-list-item-sub-item__txt">APPLE</a>
+                  </div>
+                  <div class="Menu-list-item-sub-item">
+                    <a href="#" class="Menu-list-item-sub-item__txt">SAMSUNG</a>
+                  </div>
+                  <div class="Menu-list-item-sub-item">
+                    <a href="#" class="Menu-list-item-sub-item__txt">XIAOMI</a>
+                  </div>
+                  <div class="Menu-list-item-sub-item">
+                    <a href="#" class="Menu-list-item-sub-item__txt">OPPO</a>
+                  </div>
+                  <div class="Menu-list-item-sub-item">
+                    <a href="#" class="Menu-list-item-sub-item__txt">NOKIA</a>
+                  </div>
+                  <div class="Menu-list-item-sub-item">
+                    <a href="#" class="Menu-list-item-sub-item__txt">REALME</a>
+                  </div>
+                  <div class="Menu-list-item-sub-item">
+                    <a href="#" class="Menu-list-item-sub-item__txt">VSMART</a>
+                  </div>
+                </div>
+              </div>
             </div>
             <div class="Menu-list-item">
               <div class="Menu-list-item-content">
-                <div class="Menu-list-item-content__ico"><i class="fas fa-mobile-alt"></i></div>
+                <div class="Menu-list-item-content__ico"><i class="fa fa-tablet"></i></div>
                 <p class="Menu-list-item-content__txt">TABLET</p>
               </div>
               <div class="Menu-list-item-sub">
@@ -47,25 +76,22 @@ require_once DOC_ROOT.'/common/php/common.php';
                     <a href="#" class="Menu-list-item-sub-item__txt">iPad Pro</a>
                   </div>
                   <div class="Menu-list-item-sub-item">
-                    <a href="#" class="Menu-list-item-sub-item__txt">iPad Pro</a>
+                    <a href="#" class="Menu-list-item-sub-item__txt">iPad 10.2</a>
                   </div>
                   <div class="Menu-list-item-sub-item">
-                    <a href="#" class="Menu-list-item-sub-item__txt">iPad Pro</a>
+                    <a href="#" class="Menu-list-item-sub-item__txt">iPad Air</a>
                   </div>
                   <div class="Menu-list-item-sub-item">
-                    <a href="#" class="Menu-list-item-sub-item__txt">iPad Pro</a>
+                    <a href="#" class="Menu-list-item-sub-item__txt">iPad 9.7</a>
                   </div>
                   <div class="Menu-list-item-sub-item">
-                    <a href="#" class="Menu-list-item-sub-item__txt">iPad Pro</a>
+                    <a href="#" class="Menu-list-item-sub-item__txt">iPad Mini</a>
                   </div>
                   <div class="Menu-list-item-sub-item">
-                    <a href="#" class="Menu-list-item-sub-item__txt">iPad Pro</a>
+                    <a href="#" class="Menu-list-item-sub-item__txt">Huawei</a>
                   </div>
                   <div class="Menu-list-item-sub-item">
-                    <a href="#" class="Menu-list-item-sub-item__txt">iPad Pro</a>
-                  </div>
-                  <div class="Menu-list-item-sub-item">
-                    <a href="#" class="Menu-list-item-sub-item__txt">iPad Pro</a>
+                    <a href="#" class="Menu-list-item-sub-item__txt">Samsung</a>
                   </div>
                 </div>
               </div>
@@ -74,6 +100,29 @@ require_once DOC_ROOT.'/common/php/common.php';
               <div class="Menu-list-item-content">
                 <div class="Menu-list-item-content__ico"><i class="fas fa-mobile-alt"></i></div>
                 <p class="Menu-list-item-content__txt">PHỤ KIỆN</p>
+              </div>
+              <div class="Menu-list-item-sub">
+                <div class="Menu-list-item-sub-item">
+                  <a href="#" class="Menu-list-item-sub-item__txt">Pin dự phòng</a>
+                </div>
+                <div class="Menu-list-item-sub-item">
+                  <a href="#" class="Menu-list-item-sub-item__txt">Cáp sạc</a>
+                </div>
+                <div class="Menu-list-item-sub-item">
+                  <a href="#" class="Menu-list-item-sub-item__txt">Dán màn hình</a>
+                </div>
+                <div class="Menu-list-item-sub-item">
+                  <a href="#" class="Menu-list-item-sub-item__txt">Ốp lưng</a>
+                </div>
+                <div class="Menu-list-item-sub-item">
+                  <a href="#" class="Menu-list-item-sub-item__txt">Thẻ nhớ</a>
+                </div>
+                <div class="Menu-list-item-sub-item">
+                  <a href="#" class="Menu-list-item-sub-item__txt">Chuột|Bàn phím</a>
+                </div>
+                <div class="Menu-list-item-sub-item">
+                  <a href="#" class="Menu-list-item-sub-item__txt">Camera</a>
+                </div>
               </div>
             </div>
             <div class="Menu-list-item">
@@ -137,6 +186,34 @@ require_once DOC_ROOT.'/common/php/common.php';
               </div>
             </div>
           </div>
+        </div>
+      </div>
+    </header>
+    <header class="only-sp">
+      <div class="js-nav Nav">
+        <div class="Nav-content">
+          <div class="Nav__logo">
+            <img src="/common/img/logo.webp" alt="cellphones">
+          </div>
+          <div class="Nav-search">
+            <div class="Nav-search-content">
+              <div class="Nav-search__input">
+                <input type="text" placeholder="Tìm kiếm">
+              </div>
+              <div class="Nav-search__btn">
+                <i class="fa fa-search"></i>
+              </div>
+            </div>
+          </div>
+          <div class="Nav-hamburger">
+            <div class="js-navHamburger Nav-hamburger-content">
+              <span class="Nav-hamburger-content__ico"></span>
+              <span class="Nav-hamburger-content__ico"></span>
+              <span class="Nav-hamburger-content__ico"></span>
+            </div>
+          </div>
+        </div>
+        <div class="Nav-menu">
         </div>
       </div>
     </header>
--- top/js/script.js
+++ top/js/script.js
@@ -1,5 +1,20 @@
 (function($) {
   $(function() {
 
+    $('.js-locationBtn').click(function(){
+      var mParent = $(this).parent();
+      mParent.toggleClass('Menu-branch-search-location--active');
+    });
+
+    $('.js-locationOption').click(function(){
+      var mLocation = $(this).find('span').text();
+      var locationBtn = $('.js-locationBtn');
+      locationBtn.text(mLocation);
+      locationBtn.parent().removeClass('Menu-branch-search-location--active');
+    });
+
+    $('.js-navHamburger').click(function(){
+      $('.js-nav').toggleClass('Nav--active');
+    });
   });
 })(jQuery);
