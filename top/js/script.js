0935d3a466cd1695638a267fc24dfac432e345c2
Evan <evanngo93@gmail.com>
2019-11-06 01:02:37 +0900

    Buoi3 Hoàn thành menu SP/PC

--- top/js/script.js
+++ top/js/script.js
@@ -15,6 +15,11 @@
 
     $('.js-navHamburger').click(function(){
       $('.js-nav').toggleClass('Nav--active');
+      if ($('.js-nav').hasClass('Nav--active')) {
+        $('body').addClass('Noscroll');
+      } else {
+        $('body').removeClass('Noscroll');
+      }
     });
   });
 })(jQuery);
