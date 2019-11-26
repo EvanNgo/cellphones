70b5fd91028746e9a003c13ea3ec5b7df93e8224
Evan <evanngo93@gmail.com>
2019-11-26 21:02:29 +0900

    buoi10

--- index.php
+++ index.php
@@ -71,6 +71,129 @@ require_once DOC_ROOT.'/common/php/common.php';
         </div>
       </div>
     </div>
+    <!-- Item list -->
+    <div class="Responsive">
+      <div class="Product">
+        <div class="Product-head">
+          <div class="Product-head__txt">
+            <a href="#">Bán chạy</a>
+          </div>
+          <div class="Product-head-tag">
+            <p>iPhone</p>
+            <p>Samsung</p>
+            <p>Xiaomi</p>
+            <p>OPPO</p>
+            <p>Nokia</p>
+            <p>Realme</p>
+            <p>Vsmart</p>
+          </div>
+        </div>
+        <div class="Product-content">
+          <div class="Product-content-list">
+            <div class="Product-content-item">
+              <div class="Product-content-item__img">
+                <img src="/top/img/product_item_img.webp" alt="">
+              </div>
+              <div class="Product-content-item-detail">
+                <p class="Product-content-item-detail__name">iPhone 11 Chính hãng (VN/A)</p>
+                <p class="Product-content-item-detail__price">23.000.000 đ<span>28.000.000 đ</span></p>
+                <p class="Product-content-item-detail__txt">Thu cũ đổi mới iphone 11 | pro | pro max - giá thu tốt nhất thị trường và <strong>1 km</strong> khác</p>
+              </div>
+            </div>
+            <div class="Product-content-item">
+              <div class="Product-content-item__img">
+                <img src="/top/img/product_item_img.webp" alt="">
+              </div>
+              <div class="Product-content-item-detail">
+                <p class="Product-content-item-detail__name">iPhone 11 Chính hãng (VN/A)</p>
+                <p class="Product-content-item-detail__price">23.000.000 đ<span>28.000.000 đ</span></p>
+                <p class="Product-content-item-detail__txt">Thu cũ đổi mới iphone 11 | pro | pro max - giá thu tốt nhất thị trường và <strong>1 km</strong> khác</p>
+              </div>
+            </div>
+            <div class="Product-content-item">
+              <div class="Product-content-item__img">
+                <img src="/top/img/product_item_img.webp" alt="">
+              </div>
+              <div class="Product-content-item-detail">
+                <p class="Product-content-item-detail__name">iPhone 11 Chính hãng (VN/A)</p>
+                <p class="Product-content-item-detail__price">23.000.000 đ<span>28.000.000 đ</span></p>
+                <p class="Product-content-item-detail__txt">Thu cũ đổi mới iphone 11 | pro | pro max - giá thu tốt nhất thị trường và <strong>1 km</strong> khác</p>
+              </div>
+            </div>
+            <div class="Product-content-item">
+              <div class="Product-content-item__img">
+                <img src="/top/img/product_item_img.webp" alt="">
+              </div>
+              <div class="Product-content-item-detail">
+                <p class="Product-content-item-detail__name">iPhone 11 Chính hãng (VN/A)</p>
+                <p class="Product-content-item-detail__price">23.000.000 đ<span>28.000.000 đ</span></p>
+                <p class="Product-content-item-detail__txt">Thu cũ đổi mới iphone 11 | pro | pro max - giá thu tốt nhất thị trường và <strong>1 km</strong> khác</p>
+              </div>
+            </div>
+            <div class="Product-content-item">
+              <div class="Product-content-item__img">
+                <img src="/top/img/product_item_img.webp" alt="">
+              </div>
+              <div class="Product-content-item-detail">
+                <p class="Product-content-item-detail__name">iPhone 11 Chính hãng (VN/A)</p>
+                <p class="Product-content-item-detail__price">23.000.000 đ<span>28.000.000 đ</span></p>
+                <p class="Product-content-item-detail__txt">Thu cũ đổi mới iphone 11 | pro | pro max - giá thu tốt nhất thị trường và <strong>1 km</strong> khác</p>
+              </div>
+            </div>
+            <div class="Product-content-item">
+              <div class="Product-content-item__img">
+                <img src="/top/img/product_item_img.webp" alt="">
+              </div>
+              <div class="Product-content-item-detail">
+                <p class="Product-content-item-detail__name">iPhone 11 Chính hãng (VN/A)</p>
+                <p class="Product-content-item-detail__price">23.000.000 đ<span>28.000.000 đ</span></p>
+                <p class="Product-content-item-detail__txt">Thu cũ đổi mới iphone 11 | pro | pro max - giá thu tốt nhất thị trường và <strong>1 km</strong> khác</p>
+              </div>
+            </div>
+            <div class="Product-content-item">
+              <div class="Product-content-item__img">
+                <img src="/top/img/product_item_img.webp" alt="">
+              </div>
+              <div class="Product-content-item-detail">
+                <p class="Product-content-item-detail__name">iPhone 11 Chính hãng (VN/A)</p>
+                <p class="Product-content-item-detail__price">23.000.000 đ<span>28.000.000 đ</span></p>
+                <p class="Product-content-item-detail__txt">Thu cũ đổi mới iphone 11 | pro | pro max - giá thu tốt nhất thị trường và <strong>1 km</strong> khác</p>
+              </div>
+            </div>
+            <div class="Product-content-item">
+              <div class="Product-content-item__img">
+                <img src="/top/img/product_item_img.webp" alt="">
+              </div>
+              <div class="Product-content-item-detail">
+                <p class="Product-content-item-detail__name">iPhone 11 Chính hãng (VN/A)</p>
+                <p class="Product-content-item-detail__price">23.000.000 đ<span>28.000.000 đ</span></p>
+                <p class="Product-content-item-detail__txt">Thu cũ đổi mới iphone 11 | pro | pro max - giá thu tốt nhất thị trường và <strong>1 km</strong> khác</p>
+              </div>
+            </div>
+            <div class="Product-content-item">
+              <div class="Product-content-item__img">
+                <img src="/top/img/product_item_img.webp" alt="">
+              </div>
+              <div class="Product-content-item-detail">
+                <p class="Product-content-item-detail__name">iPhone 11 Chính hãng (VN/A)</p>
+                <p class="Product-content-item-detail__price">23.000.000 đ<span>28.000.000 đ</span></p>
+                <p class="Product-content-item-detail__txt">Thu cũ đổi mới iphone 11 | pro | pro max - giá thu tốt nhất thị trường và <strong>1 km</strong> khác</p>
+              </div>
+            </div>
+            <div class="Product-content-item">
+              <div class="Product-content-item__img">
+                <img src="/top/img/product_item_img.webp" alt="">
+              </div>
+              <div class="Product-content-item-detail">
+                <p class="Product-content-item-detail__name">iPhone 11 Chính hãng (VN/A)</p>
+                <p class="Product-content-item-detail__price">23.000.000 đ<span>28.000.000 đ</span></p>
+                <p class="Product-content-item-detail__txt">Thu cũ đổi mới iphone 11 | pro | pro max - giá thu tốt nhất thị trường và <strong>1 km</strong> khác</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <?= insertJS('/top/js/script.js'); ?>
   </body>
 </html>
